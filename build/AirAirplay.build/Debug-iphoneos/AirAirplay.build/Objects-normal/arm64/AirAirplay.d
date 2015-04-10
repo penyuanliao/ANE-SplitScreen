3771273714cdd9e53618a4c6816342f4a575c73c
mobile <twofishsman@gmail.com>
2015-04-10 09:45:01 +0800

    library

--- build/AirAirplay.build/Debug-iphoneos/AirAirplay.build/Objects-normal/arm64/AirAirplay.d
+++ build/AirAirplay.build/Debug-iphoneos/AirAirplay.build/Objects-normal/arm64/AirAirplay.d
@@ -2,7 +2,7 @@ dependencies: \
   /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/AirAirplay.m \
   /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/FlashRuntimeExtensions.h \
   /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/AirAirplay.h \
-  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/ScreenConnect.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/SplitScreen.h \
   /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/iRTMPPlayer.h \
   /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libavformat/avformat.h \
   /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libavcodec/avcodec.h \
@@ -31,4 +31,36 @@ dependencies: \
   /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libavformat/avio.h \
   /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libavformat/version.h \
   /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libswscale/swscale.h \
-  /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libswscale/version.h
+  /Users/mobile/Documents/benson/ANE-SplitScreen/Frameworks/ffmpeg/include/libswscale/version.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/NetStream/StreamDecoder/RTMPDecoder.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/NetStream/OpenGL.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/NetStream/frameYUV.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/NetStream/GLSLRenderYUV.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/OpenGLES.framework/Headers/ES2/gl.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/OpenGLES.framework/Headers/ES2/glext.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKit.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKitBase.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKView.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKViewController.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffects.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKNamedEffect.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKBaseEffect.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffectPropertyTransform.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffectProperty.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKMath.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKMathTypes.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKMatrix3.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKVector3.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKQuaternion.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKVector4.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKMatrix4.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKVector2.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKMatrixStack.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKMathUtils.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffectPropertyLight.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffectPropertyMaterial.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffectPropertyTexture.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKEffectPropertyFog.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKReflectionMapEffect.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKSkyboxEffect.h \
+  /Users/mobile/Documents/benson/ANE-SplitScreen/ANECompiler/libs/GLKit.framework/Headers/GLKTextureLoader.h
--- build/AirAirplay.build/Debug-iphoneos/AirAirplay.build/Objects-normal/armv7/ScreenConnect.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/ScreenConnect.m \
-  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/ScreenConnect.h \
-  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/FlashRuntimeExtensions.h \
-  /Users/mobile/Documents/benson/ANE-SplitScreen/AirAirplay/AirAirplay.h
